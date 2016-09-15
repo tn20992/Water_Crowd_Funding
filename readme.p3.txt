4a7f70edbe7ada0faab65ce222714437d579be32
Ryan Voor <ryan.voor@gmail.com>
2016-09-14 20:50:32 -0400

    Removed traces of M2 and practice stuff. Set up repository for actual project :D

--- readme.p3.txt
+++
@@ -1,2 +0,0 @@
-Bharath Kalidindi
-bharkal@gatech.edu
---
+++ src/main/java/fxapp/MainFXApplication.java
@@ -0,0 +1,7 @@
+package fxapp;
+
+public class MainFXApplication {
+    public static void main(String[] args) {
+        System.out.println("You ran it huzzah");
+    }
+}

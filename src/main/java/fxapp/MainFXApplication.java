65cab003bbfa89e7529ea1c60ed3c354e4bd8018
Ryan Voor <ryan.voor@gmail.com>
2016-09-15 09:49:04 -0400

    Set up some boiler plate code for getting the project started

--- src/main/java/fxapp/MainFXApplication.java
+++ src/main/java/fxapp/MainFXApplication.java
@@ -1,7 +1,73 @@
 package fxapp;
 
-public class MainFXApplication {
-    public static void main(String[] args) {
-        System.out.println("You ran it huzzah");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import controller.WelcomeScreenController;
+
+public class MainFXApplication extends Application {
+
+    /** the main container for the application window */
+    private Stage mainScreen;
+
+    /** the main layout for the main window */
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        mainScreen = primaryStage;
+        initRootLayout(mainScreen);
     }
+
+    /**
+     * return a reference to the main window stage
+     * @return reference to main stage
+     * */
+    public Stage getMainScreen() { return mainScreen; }
+
+    /**
+     * Initialize the main screen for the application.  Most other views will be shown in this screen.
+     *
+     * @param mainScreen  the main Stage window of the application
+     */
+    private void initRootLayout(Stage mainScreen) {
+        try {
+
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainFXApplication.class.getResource("../view/WelcomeScreen.fxml"));
+            rootLayout = loader.load();
+
+            // Give the controller access to the main app.
+            WelcomeScreenController controller = loader.getController();
+            controller.setMainApp(this);
+
+            // Set the Main App title
+            mainScreen.setTitle("Welcome!");
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            mainScreen.setScene(scene);
+            mainScreen.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * main method for main application class
+     * @param args command line parameters
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }

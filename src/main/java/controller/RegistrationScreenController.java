c12006f4898f11032bc877191b7a21c4d2280e95
hdo33 <hdo33@gatech.edu>
2016-09-15 22:34:48 -0400

    added UI fxml and controller

---
+++ src/main/java/controller/RegistrationScreenController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import fxapp.MainFXApplication;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Alert;
+
+/**
+ * The controller for the root/main window
+ *
+ */
+public class RegistrationScreenController {
+
+    /** reference back to mainApplication if needed */
+    private MainFXApplication mainApplication;
+
+    /**
+     * allow for calling back to the main application code if necessary
+     * @param main   the reference to the FX Application instance
+     * */
+    public void setMainApp(MainFXApplication main) {
+        mainApplication = main;
+    }
+
+    /**
+     * Login Button in Login Screen
+     */
+    @FXML
+    private void regButtonRegPressed() {
+
+
+    }
+
+    /**
+     * Cancel button in Login Screen
+     */
+    @FXML
+    private void cancelButtonRegPressed() {
+
+
+    }
+
+}

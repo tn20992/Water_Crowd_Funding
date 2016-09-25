0bf50fe4463eb9f2b48033af975aaf6f505790d4
bkalidindi3 <bharkal@gatech.edu>
2016-09-25 16:09:32 -0400

    added alters, made changes in corresponding meta files and populate code

--- src/main/sql/populate/populate_tb_entity.sql
+++ src/main/sql/populate/populate_tb_entity.sql
@@ -1,22 +1,37 @@
 INSERT INTO tb_entity
 (
     username,
-    password
+    password,
+    name,
+    email,
+    street_address
 )
 VALUES
 (
     'user1',
-    'password1'
+    'password1',
+    'U1',
+    'u1@gmail.com',
+    '123 U1 Road'
 ),
 (
     'user2',
-    'password2'
+    'password2',
+    'U2',
+    'u2@gmail.com',
+    '143 U2 Road'
 ),
 (
     'user3',
-    'password3'
+    'password3',
+    'U3',
+    'u3@gmail.com',
+    '163 U3 Road'
 ),
 (
     'user4',
-    'password4'
+    'password4',
+    'U4',
+    'u4@gmail.com',
+    '183 U4 Road'
 );

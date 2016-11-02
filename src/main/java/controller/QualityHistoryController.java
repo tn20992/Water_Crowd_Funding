00603bb21633acf0b994b5b928490ba1667ff938
hdo33 <hdo33@gatech.edu>
2016-11-01 20:47:42 -0400

    fixed

--- src/main/java/controller/QualityHistoryController.java
+++ src/main/java/controller/QualityHistoryController.java
@@ -1,7 +1,9 @@
 package controller;
 import fxapp.MainFXApplication;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
@@ -15,6 +17,8 @@ import model.User;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 
 /**
@@ -24,6 +28,7 @@ public class QualityHistoryController {
 
     private MainFXApplication mainApplication;
     private Facade facade = Facade.getFacade();
+    private User user;
     private double longitude;
     private double latitude;
     private int year;
@@ -50,7 +55,7 @@ public class QualityHistoryController {
     @FXML
     private NumberAxis yAxis;
 
-    private String[] virusOrContaminantList = {"VirusPPM", "ContaminantPPM"};
+    private String[] virusOrContaminantList = {"VirusPPM","ContaminantPPM"};
 
     @FXML
     private void initialize() {
@@ -68,12 +73,9 @@ public class QualityHistoryController {
     }
 
     @FXML
-    @SuppressWarnings("unchecked")
-
-    public void initGraph(Location location, Integer year, String vOrC
-            , ArrayList<Point> points) {
-        chartHistorical.setTitle("Historical Chart of " + vOrC + " in "
-                + String.valueOf(year));
+    public void initGraph(Location location, Integer year, String vOrC, ArrayList<Point> points) {
+        chartHistorical.getData().clear();
+        chartHistorical.setTitle("Historical Chart of " + vOrC + " in " + String.valueOf(year));
         xAxis.setLabel("Months");
         xAxis.setLowerBound(1);
         xAxis.setUpperBound(12);
@@ -82,15 +84,14 @@ public class QualityHistoryController {
         XYChart.Series series1 = new XYChart.Series();
         series1.setName(vOrC);
 
-        for(Point i : points) {
+        for(Point i:points) {
             Timestamp ts = i.getTime();
-            int month = Integer.parseInt(ts.toString().substring(5, 7));
+            int month = Integer.parseInt(ts.toString().substring(5,7));
 
             if (vOrC.equals("VirusPPM")) {
                 series1.getData().add(new XYChart.Data(month, i.getVirusPPM()));
             } else {
-                series1.getData().add(new XYChart.Data(month
-                        , i.getContaminantPPM()));
+                series1.getData().add(new XYChart.Data(month, i.getContaminantPPM()));
             }
         }
         chartHistorical.getData().addAll(series1);
@@ -118,8 +119,7 @@ public class QualityHistoryController {
                     vOrC = "ContaminantPPM";
                 }
 
-                ArrayList<Point> pointList = facade.getHistoryByLocation
-                        (location, year);
+                ArrayList<Point> pointList = facade.getHistoryByLocation(location, (int) year);
                 initGraph(location, year, vOrC, pointList);
 
 
@@ -127,8 +127,7 @@ public class QualityHistoryController {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("ERROR");
                 alert.setContentText(
-                        "Longitude or Latitude"
-                                + " or Year cannot include letters!!!");
+                        "Longitude or Latitude or Year cannot include letters!!!");
                 alert.showAndWait();
             }
         }
